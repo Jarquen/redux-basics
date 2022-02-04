500e242ad093840649bdbb75e320024e409d9c1e
Quentin <quentin.jarlot@gmail.com>
2022-02-04 15:57:09 +0100

    delete node-sass and add sass

--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//redux
+import {Provider} from "react-redux";
+import {applyMiddleware, createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers"
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>,
+    document.getElementById("root")
+);
